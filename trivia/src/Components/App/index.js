70d837ce79fbc8571c2b9ab3f9059cb0cace00cd
craig2812 <craig2812@gmail.com>
2021-02-19 11:22:13 +0000

    plan index and app complete

---
+++ trivia/src/Components/App/index.js
@@ -0,0 +1,37 @@
+import './index.css';
+
+
+// states
+  // visibility of pages 
+  // genre 
+  // difficulty 
+  // player Name 
+
+//functions to manipulate WelcomePage
+  //handleGenre 
+  //handleDifficulty
+  //handleName 
+  //handleVisibility
+
+//props to pass down
+  // visibility component to both WelcomePage and DisplayQuestions
+  // handleVisibility to both componentns 
+  // handleGenre & handleDifficulty & handleName to Welcome page onlt
+  // Pass all states to DisplayQuestion
+
+// return 
+  // App has 2 components - DisplayQuestions and WelcomePage
+
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+
+
+export default App;
+
---
+++ trivia/src/Components/Button/index.js
@@ -0,0 +1,17 @@
+// state 
+    // object: property of difficultyState/genreState & toggle 
+        // {medium : true} / {General: true}
+    // buttonUseability:  button allowed to work - true/false
+
+
+//function 
+    //handleToggle - set the states to true/false 
+    //handleUseability - turn off the other buttons 
+
+// props to receive  
+    // Text (answer/Difficulty/Genre)
+
+// Return 
+    //handleToggle function (handles on / off onClick)
+
+{/* <button onClick={handleToggle}><</button> */}
