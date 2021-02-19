bb2945b286012b321e5c26b6b1bf5b3873480107
Roxana Tot <roxana.tot@gmail.com>
2021-02-19 14:36:29 +0000

    Diplaying questions and answers

---
+++ trivia/src/Components/DisplayAnswers/index.js
@@ -0,0 +1,11 @@
+function DisplayAnswers({state, dispatch, correctAnswer, incorrectAnswers}) {
+    // store answers in an array
+    // shuffle array order
+    return(
+        <div>
+            <button></button>
+        </div>
+    )
+}
+
+export default DisplayAnswers;
